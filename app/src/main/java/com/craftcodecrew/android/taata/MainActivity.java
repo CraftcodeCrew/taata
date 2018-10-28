be4590dc3dee6c29bcaaffca8d109348d6cc3999
soeren <elias.soeren@gmail.com>
2018-10-28 09:00:23 +0100

    Switcher done
    next step is the ListView of Insurables

--- app/src/main/java/com/craftcodecrew/android/taata/MainActivity.java
+++ app/src/main/java/com/craftcodecrew/android/taata/MainActivity.java
@@ -5,15 +5,21 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.location.Location;
 import android.location.LocationListener;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import android.support.annotation.StyleRes;
+import android.widget.ViewSwitcher;
 
 import com.craftcodecrew.android.taata.cards.SliderAdapter;
 import com.ramotion.cardslider.CardSliderLayoutManager;
@@ -43,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private String[] categoryDescriptions;
     private int[] categoryImageId;
     private List<List<Insurable>> categorysInsurables;
-    private String[] categoryCoverage = {"80/% safe", "56/% safe","25/% safe","90/% safe","72/%","80/%","95/%"};
+    private String[] categoryCoverage = {"80% safe", "56% safe","25% safe","90% safe","72%","80%","95%"};
 
     private TextSwitcher categoryNameSwitcher;
     private TextSwitcher categoryDescriptionSwitcher;
@@ -97,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
                 categoryNames[i] = categoriesList.get(i).getTitle();
                 categoryDescriptions[i] = categoriesList.get(i).getDescription();
-                categorysInsurables.set(i,categoriesList.get(i).getInsurables());
+                //categorysInsurables.set(i,categoriesList.get(i).getInsurables());
 
                 String drawableName = categoriesList.get(i).getImageId();
                 categoryImageId[i] = getResources()
@@ -119,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         initRecyclerView();
+        initSwitchers();
         imageView.setVisibility(View.GONE);
     }
 
@@ -182,6 +189,21 @@ public class MainActivity extends AppCompatActivity {
         new CardSnapHelper().attachToRecyclerView(recyclerView);
     }
 
+    private void initSwitchers(){
+        categoryNameSwitcher = (TextSwitcher) findViewById(R.id.category_title);
+        categoryNameSwitcher.setFactory(new TextViewFactory(R.style.categoryTitleStyle,false));
+        categoryNameSwitcher.setCurrentText(categoryNames[0]);
+
+        categoryDescriptionSwitcher = (TextSwitcher) findViewById(R.id.category_description);
+        categoryDescriptionSwitcher.setFactory(new TextViewFactory(R.style.categoryDescriptionStyle,false));
+        categoryDescriptionSwitcher.setCurrentText(categoryDescriptions[0]);
+
+        categoryCoverageSwitcher = (TextSwitcher) findViewById(R.id.category_coverage);
+        categoryCoverageSwitcher.setFactory(new TextViewFactory(R.style.categoryCoverageStyle,false));
+        categoryCoverageSwitcher.setCurrentText(categoryCoverage[0]);
+    }
+
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -208,7 +230,21 @@ public class MainActivity extends AppCompatActivity {
             animV[1] = R.anim.slide_out_top;
         }
 
+        categoryNameSwitcher.setInAnimation(MainActivity.this, animH[0]);
+        categoryNameSwitcher.setOutAnimation(MainActivity.this, animH[1]);
+        categoryNameSwitcher.setText(categoryNames[pos%categoryNames.length]);
+
+
+        categoryDescriptionSwitcher.setInAnimation(MainActivity.this, animH[0]);
+        categoryDescriptionSwitcher.setOutAnimation(MainActivity.this, animH[1]);
+        categoryDescriptionSwitcher.setText(categoryDescriptions[pos%categoryDescriptions.length]);
+
+        categoryCoverageSwitcher.setInAnimation(MainActivity.this, animH[0]);
+        categoryCoverageSwitcher.setOutAnimation(MainActivity.this, animH[1]);
+        categoryCoverageSwitcher.setText(categoryCoverage[pos%categoryCoverage.length]);
+
         currentPosition = pos;
+
     }
 
     private class OnCardClickListener implements View.OnClickListener {
@@ -234,4 +270,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // everything for the switchers
+    private class TextViewFactory implements  ViewSwitcher.ViewFactory {
+
+        @StyleRes final int styleId;
+        final boolean center;
+
+        TextViewFactory(@StyleRes int styleId, boolean center) {
+            this.styleId = styleId;
+            this.center = center;
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public View makeView() {
+            final TextView textView = new TextView(MainActivity.this);
+
+            if (center) {
+                textView.setGravity(Gravity.CENTER);
+            }
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                textView.setTextAppearance(MainActivity.this, styleId);
+            } else {
+                textView.setTextAppearance(styleId);
+            }
+
+            return textView;
+        }
+
+    }
 }
