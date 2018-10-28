d08e87035977d80ac4810565d3c30b6203c01cfb
soeren <elias.soeren@gmail.com>
2018-10-28 05:45:15 +0100

    some nice UI tweaks, to prepare for another bigger step

--- app/src/main/java/com/craftcodecrew/android/taata/MainActivity.java
+++ app/src/main/java/com/craftcodecrew/android/taata/MainActivity.java
@@ -104,9 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         initRecyclerView();
-
         imageView.setVisibility(View.GONE);
-
     }
 
 
